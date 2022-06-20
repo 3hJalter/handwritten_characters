e49e058d5452c7004b7b55485e344e8d6ffca979
Ngo_Quang_Minh <quangminhngo3@gmail.com>
2022-06-20 17:34:32 +0700

    digit CNN

--- Digit/digit_recognition.py
+++ Digit/digit_recognition.py
@@ -7,24 +7,35 @@ import keras.datasets
 import keras.utils
 import keras.models
 import keras.layers
-
-mnist = keras.datasets.mnist
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = keras.utils.normalize(x_train, axis=1)
 x_test = keras.utils.normalize(x_test, axis=1)
 
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28, 28)))
-model.add(keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(100, activation=tf.nn.relu))
+model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=10)
 
 loss, accuracy = model.evaluate(x_test, y_test)
 
@@ -33,10 +44,11 @@ print(loss)
 
 model.save('digits.model')
 
-for x in range(0, 10):
+for x in range(0, 20):
     img = cv.imread(f'D:\Project\Handwriting\Digit\{x}.png')[:, :, 0]
     img = np.invert(np.array([img]))
     prediction = model.predict(img)
     print(f'The result is:  {np.argmax(prediction)}')
     plt.imshow(img[0], cmap=plt.cm.binary)
     plt.show()
+# 17/20 correct testcases
